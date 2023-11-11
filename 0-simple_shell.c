1835808d55cb3ce8f77a239f765e0a6ba027ed45
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-11-10 17:50:03 -0800

    task 0

---
+++ 0-simple_shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Betty - Function that prints betty
+ *
+ * Return: Always 0 when success
+ */
+
+int main(void)
+{
+	printf("Hello, Betty!\n");
+	return (0);
+}
---
+++ simple_shell.h
@@ -0,0 +1,14 @@
+#ifndef "SIMPLE_SHELL_H"
+#define "SIMPLE_SHELL_H"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <main.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void);
+
+#endif
